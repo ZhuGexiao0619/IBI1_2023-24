7910ae7eeb45aec5752061146b95d9b422194f16
zgx <2533461560@qq.com>
2024-03-19 10:51:14 +0800

    Initialise

---
+++ Practical6/Average_day.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+dic1={"Sleeping":8,"Classes":6,"Studying":3.5,"TV":2,"Music":1,"other":3.5}
+Activity=["Sleeping","Classes","Studying","TV","Music","other"]
+average=[dic1["Sleeping"],dic1["Classes"],dic1["Studying"],dic1["TV"],dic1["Music"],dic1["other"]]
+plt.figure()
+plt.pie(average,labels=Activity,startangle=90)
+plt.show()
