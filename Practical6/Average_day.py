57445d543767f91648afd97262a93e65ca8f7fd3
zgx <2533461560@qq.com>
2024-03-20 14:08:00 +0800

    initialise

--- Practical6/Average_day.py
+++ Practical6/Average_day.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
-dic1={"Sleeping":8,"Classes":6,"Studying":3.5,"TV":2,"Music":1,"other":3.5}
+dic1={"Sleeping":8,"Classes":6,"Studying":3.5,"TV":2,"Music":1}
+dic1["other"]=24-dic1["Sleeping"]-dic1["Classes"]-dic1["Studying"]-dic1["TV"]-dic1["Music"]
 Activity=["Sleeping","Classes","Studying","TV","Music","other"]
 average=[dic1["Sleeping"],dic1["Classes"],dic1["Studying"],dic1["TV"],dic1["Music"],dic1["other"]]
 plt.figure()
